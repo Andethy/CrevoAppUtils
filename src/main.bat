51a5aed0b250ff017cae35692c63a73276f5ab36
Jack Hayley <jhayley426@gmail.com>
2022-03-31 15:57:33 -0400

    Starting executable support

---
+++ src/main.bat
@@ -0,0 +1,2 @@
+call "C:\Users\Jack\anaconda3\Scripts\activate.bat" Work
+pyinstaller C:\Users\Jack\Documents\GitHub\CrevoAppUtils\src\main.spec --distpath C:\Users\Jack\Documents\GitHub\CrevoAppUtils\build
